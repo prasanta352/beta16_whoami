c2e29b330c2b5bf6843310465a9f2fbf7d36d93a
Prasanta Barman <prasantabarman06@gmail.com>
2020-10-31 18:23:03 +0530

    added working sms receiver implementation

--- app/src/main/java/com/example/oflinemaphelper/Activity/Implementation/SmsImplementation.java
+++ app/src/main/java/com/example/oflinemaphelper/Activity/Implementation/SmsImplementation.java
@@ -3,19 +3,24 @@ package com.example.oflinemaphelper.Activity.Implementation;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.widget.Toast;
+import android.util.Log;
 
-import java.util.List;
+public class SmsImplementation implements BaseImplementation {
 
-public class SmsImplementation  implements BaseImplementation {
-
-    private SmsManager smsManager;
-    private String mMessage = null;
     public String mFrom = null;
     public String mTo = null;
+    private final Context mContext;
+    private final String TAG = "BaseImplementation";
+    private final String DESTINATION_MOBILE_NO = "9547466422";
+
+    public SmsImplementation(Context c) {
+        super();
+        mContext = c;
+    }
 
     @Override
     public void setLocation(String from, String to) {
@@ -29,11 +34,11 @@ public class SmsImplementation  implements BaseImplementation {
         // 2. send sms
         // 3. return sms sending status true|false
 
-        if((mFrom != null) && (mTo != null)) {
+        if ((mFrom != null) && (mTo != null)) {
 
-            String message = mFrom +" "+ mTo;
-            smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage("9547466422", null, message, null, null);
+            String message = mFrom + " " + mTo;
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(DESTINATION_MOBILE_NO, null, message, null, null);
             responseListener.onResponse(true);
         } else {
             responseListener.onResponse(false);
@@ -41,16 +46,45 @@ public class SmsImplementation  implements BaseImplementation {
     }
 
     @Override
-    public void startGettingResponse(OnGettingResponseListener responseListener) {
+    public void startGettingResponse(final OnGettingResponseListener responseListener) {
 
         // 1. wait for response sms
         // 2. on getting response sms call responseListener.onResponse with the sms
 
+        BroadcastReceiver smsReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                // Get the data (SMS data) bound to intent
+                Bundle bundle = intent.getExtras();
 
-        responseListener.onResponse(mMessage);
+                if (bundle != null) {
+                    // Retrieve the SMS Messages received
 
+                    Object[] sms = (Object[]) bundle.get("pdus");
+
+                    // For every SMS message received
+                    for (Object sm : sms) {
+                        // Convert Object array
+                        SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sm);
+
+                        String phone = smsMessage.getOriginatingAddress();
+                        String message = smsMessage.getMessageBody();
+
+                        String mMessage = phone + ": " + message;
+                        Log.d(TAG, "onReceive: " + mMessage);
+
+                        // message has been received call the call back function with the message
+                        responseListener.onResponse(mMessage);
+                    }
+                }
+
+
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        mContext.registerReceiver(smsReceiver, intentFilter);
     }
-
 
 
 }
--- app/src/main/java/com/example/oflinemaphelper/Activity/Result.java
+++ app/src/main/java/com/example/oflinemaphelper/Activity/Result.java
@@ -1,28 +1,17 @@
 package com.example.oflinemaphelper.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.wifi.WifiManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.telephony.SmsMessage;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.oflinemaphelper.Activity.Implementation.BaseImplementation;
 import com.example.oflinemaphelper.Activity.Implementation.SmsImplementation;
 import com.example.oflinemaphelper.R;
-
-import java.util.List;
 
 public class Result extends AppCompatActivity {
 
@@ -46,16 +35,14 @@ public class Result extends AppCompatActivity {
         Log.d(TAG, "onCreate: from -> " + mFrom + "  to -> " + mTo);
 
 
-
         attemptSendSms();
 
     }
 
 
-
     private void attemptSendSms() {
 
-        SmsImplementation sms = new SmsImplementation();
+        SmsImplementation sms = new SmsImplementation(this);
         sms.setLocation(mFrom, mTo);
         sms.sendRequest(new BaseImplementation.OnSendRequestResponseListener() {
             @Override
@@ -65,53 +52,19 @@ public class Result extends AppCompatActivity {
                 Log.d(TAG, "onResponse: " + mSentSMS);
             }
         });
-//        sms.startGettingResponse(new BaseImplementation.OnGettingResponseListener() {
-//            @Override
-//            public void onResponse(String responses) {
-//
-//                Log.d(TAG, "onResponse: " + responses);
-//                Toast.makeText(Result.this, responses, Toast.LENGTH_LONG).show();
-//            }
-//        });
 
-        if(mSentSMS) {
-            receiveSms();
+        if (mSentSMS) {
+            sms.startGettingResponse(new BaseImplementation.OnGettingResponseListener() {
+                @Override
+                public void onResponse(String responses) {
+                    Log.d(TAG, "onResponse: " + responses);
+                    Toast.makeText(Result.this, responses, Toast.LENGTH_LONG).show();
+                }
+            });
+
         }
 
     }
 
 
-    private void receiveSms() {
-
-        BroadcastReceiver smsReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                // Get the data (SMS data) bound to intent
-                Bundle bundle = intent.getExtras();
-
-                if (bundle != null) {
-                    // Retrieve the SMS Messages received
-
-                    Object[] sms = (Object[]) bundle.get("pdus");
-
-                    // For every SMS message received
-                    for (int i=0; i < sms.length; i++) {
-                        // Convert Object array
-                        SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
-
-                        String phone = smsMessage.getOriginatingAddress();
-                        String message = smsMessage.getMessageBody().toString();
-
-                        String mMessage = phone + ": " + message;
-                        Toast.makeText(Result.this, mMessage, Toast.LENGTH_LONG).show();
-
-
-                    }
-                }
-
-
-            }
-        };
-
-    }
 }
